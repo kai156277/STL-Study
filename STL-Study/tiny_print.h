8f9dfffbf15370da10fdb31966d5f8f1e45576a2
zhaokai <1005324930@qq.com>
2018-07-28 08:57:38 +0800

    添加：序列容器的输出模块
    1. 模仿STL的编译时指定调用的方式，区分不同迭代器类型的容器输出方式。
    2. 支持使用"仿函数(特化 item_printf 类重新实现 operator() )"和
       "重载operator<<()"的方式指定输出内容。

---
+++ STL-Study/tiny_print.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "tiny_vector.h"
+#include <iostream>
+
+template<typename T>
+struct item_printf
+{
+	void operator()(const T& item)
+	{
+		std::cout << item << " ";
+	}
+};
+
+template<typename T, 
+	template<typename T, typename Alloc = tiny::alloc > class VECTOR = tiny::vector>
+void STL_printf(const VECTOR<T>& vec)
+{
+#ifdef MORE_DEBUG_INFO
+	printf("void vector_printf(const %s<%s>&)... \n", typeid(VECTOR).name(), typeid(T).name());
+#endif
+	printf_by_iter_type(vec, item_printf<T>(), iterator_category(vec.cbegin()));
+}
+
+template<typename T>
+void printf_by_iter_type(const T& container, item_printf<typename T::value_type> print, tiny::random_access_iterator_tag)
+{
+	for (size_t i = 0; i < container.size(); ++i)
+		print(container[i]);
+	putchar('\n');
+}
+
+template<typename T>
+void printf_by_iter_type(const T& container, item_printf<typename T::value_type> print, tiny::input_iterator_tag)
+{
+	for (auto iter = vec.cbegin(); iter != vec.cend(); iter++)
+		print(*iter);
+	putchar('\n');
+}
+
